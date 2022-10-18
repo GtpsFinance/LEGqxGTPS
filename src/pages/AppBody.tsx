20dd850aaca1ff23c938dd3b7acb540762999843
Gtps.Finance <welcome@gtps.finance>
2022-10-18 01:45:07 +0100

    f

--- src/pages/AppBody.tsx
+++ src/pages/AppBody.tsx
@@ -4,13 +4,13 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 export const BodyWrapper = styled.div`
   position: relative;
-  max-width: 600px;
+  max-width: 800px;
   width: 100%;
   background: ${({ theme }) => theme.bg1};
   box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.01), 0px 4px 8px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.04),
     0px 24px 32px rgba(0, 0, 0, 0.01);
-  border-radius: 30px;
-  padding: 1rem;
+  border-radius: 500px;
+  padding: 0rem;
 `
 
 /**
